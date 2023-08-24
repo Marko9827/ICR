9e18b6f2d14a7542539560d0b3ddc1163a526951
Marko Nikolic <marko.supergun@gmail.com>
2023-08-24 21:44:19 +0200

    fx3

--- f_b/app/app.php
+++ f_b/app/app.php
@@ -220,18 +220,20 @@ class ICR
         if (mysqli_num_rows($sql) > 0) {
             $row = mysqli_fetch_assoc($sql);
             if ($row['status'] == 0) { //predstojeci coming son
-                echo '<rezerved><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512" style="
+                echo '<rezerved data-ui="'.$row["status"].'">   
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512" style="
             fill: white !important;
             margin-right: 10px;
         "><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M381 114.9L186.1 41.8c-16.7-6.2-35.2-5.3-51.1 2.7L89.1 67.4C78 73 77.2 88.5 87.6 95.2l146.9 94.5L136 240 77.8 214.1c-8.7-3.9-18.8-3.7-27.3 .6L18.3 230.8c-9.3 4.7-11.8 16.8-5 24.7l73.1 85.3c6.1 7.1 15 11.2 24.3 11.2H248.4c5 0 9.9-1.2 14.3-3.4L535.6 212.2c46.5-23.3 82.5-63.3 100.8-112C645.9 75 627.2 48 600.2 48H542.8c-20.2 0-40.2 4.8-58.2 14L381 114.9zM0 480c0 17.7 14.3 32 32 32H608c17.7 0 32-14.3 32-32s-14.3-32-32-32H32c-17.7 0-32 14.3-32 32z"></path></svg>You have already booked a ticket</rezerved>';
             }
             if ($row['status'] == 1) { //Canceled
-                echo '<rezerved>  <i style="
+                echo '<rezerved data-ui="'.$row["status"].'">  <i style="
                 fill: white !important;
                 margin-right: 10px;" class="bi bi-trash"></i>Ticket canceled</rezerved>';
             }
             if ($row['status'] == 2) { // complete obaljvne
-                echo '<rezerved><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512" style="
+                echo '<rezerved data-ui="'.$row["status"].'">  
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512" style="
             fill: white !important;
             margin-right: 10px;
         "><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M381 114.9L186.1 41.8c-16.7-6.2-35.2-5.3-51.1 2.7L89.1 67.4C78 73 77.2 88.5 87.6 95.2l146.9 94.5L136 240 77.8 214.1c-8.7-3.9-18.8-3.7-27.3 .6L18.3 230.8c-9.3 4.7-11.8 16.8-5 24.7l73.1 85.3c6.1 7.1 15 11.2 24.3 11.2H248.4c5 0 9.9-1.2 14.3-3.4L535.6 212.2c46.5-23.3 82.5-63.3 100.8-112C645.9 75 627.2 48 600.2 48H542.8c-20.2 0-40.2 4.8-58.2 14L381 114.9zM0 480c0 17.7 14.3 32 32 32H608c17.7 0 32-14.3 32-32s-14.3-32-32-32H32c-17.7 0-32 14.3-32 32z"></path></svg>You have already booked a ticket</rezerved>';
@@ -284,7 +286,11 @@ class ICR
                         <div class="card_body_grid d-flex justify-content-between align-items-center">
                             <div class="btn-group">
                                 <a type="button" class="btn btn-sm btn-outline-secondary" href="<?php echo "./?p=flight&id=$row[flight_id]"; ?>">Info</a>
+                             <?php
+                             
+                             if ($row['status'] == 0) {?>
                                 <a type="button" class="btn btn-sm btn-outline-secondary" href="<?php echo "./?p=ticket&what=edit&id=$row[rezerved_id]"; ?>"><i class="bi bi-pencil margin-right-10"></i>Edit</a>
+         <?php } ?>
                             </div>
                             <small style="text-align: left;" class="text-muted"><?php
                                                                                 echo "$row[time_start] - $row[time_end] | " . $this->cuva_idf("price", "$row[flight_id]") . "$";
--- f_b/app/pages/journal.php
+++ f_b/app/pages/journal.php
@@ -1,16 +1,27 @@
-<main role="main">
-    <img src=".//?f=wallp/254381.png" alt="aer" loading="lazy" class="background_image" />
+<?php if ($this->isLoged()) {
 
 
-    <div class="album py-5  ">
-        <div class="container" data-type="journal_all">
+?><main role="main">
+        <img src=".//?f=wallp/254381.png" alt="aer" loading="lazy" class="background_image" />
 
-            <div class="row">
-                <?php
-                $this->flights_card_f("SELECT * FROM rezerved WHERE rezerved.user_id = $_SESSION[user_id]  ORDER BY  rezerved.time ASC");
-                ?>
+
+        <div class="album py-5  ">
+            <div class="container" data-type="journal_all">
+
+                <div class="row">
+                    <?php
+                    $this->flights_card_f("SELECT * FROM rezerved WHERE rezerved.user_id = $_SESSION[user_id]  ORDER BY  rezerved.time DESC");
+                    ?>
+                </div>
             </div>
         </div>
-    </div>
 
-</main>
+    </main> <?php } else {
+            ?>
+    <script>
+        document.body.onload = function() {
+            ICR.ui.modal('login_modal');
+        }
+    </script>
+<?php
+        } ?>
