64b6a0bc2208f975c868e47d6308c4b8d32d342a
Marko Nikolic <marko.supergun@gmail.com>
2023-08-26 10:13:50 +0200

    Update db.php

--- f_b/app/conf/db.php
+++ f_b/app/conf/db.php
@@ -2,5 +2,5 @@
  
 define("DB_SERVER", "localhost");
 define("DB_username", "root");
-define("DB_password", "Nikolicmarko9827**");
-define("DB_DBname", "icr");
+define("DB_password", "Your password");
+define("DB_DBname", "Your database");
